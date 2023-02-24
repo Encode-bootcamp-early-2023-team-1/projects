6ce673cbb27c7e866674516539e77daac386283a
edwinteo96 <edwinteo96@gmail.com>
2023-02-25 04:23:44 +0800

    Update 01_Deployment.ts

--- weekend02/scripts/01_Deployment.ts
+++ weekend02/scripts/01_Deployment.ts
@@ -16,42 +16,41 @@ function convertStringArrayToBytes32(array: string[]) {
 
 async function main() {
     const args = process.argv;
-    const proposals = args.slice(2);
-    if (proposals.length <= 0) throw new Error("Missing parameters: proposals");
+    
+    const PROPOSALS = args.slice(2);
 
-    const provider = new ethers.providers.InfuraProvider(
-        "goerli",
-        process.env.INFURA_API_KEY
-    );
+    if (PROPOSALS.length <= 0) {
+        throw new Error("Missing parameter : proposals");
+    }
+    
+    const pkey=  process.env.PRIVATE_KEY;
 
-    const privateKey = process.env.PRIVATE_KEY;
-    if (!privateKey || privateKey.length <= 0)
-        throw new Error("Missing environment: Mnemonic seed");
-    const wallet = new ethers.Wallet(privateKey);
-    console.log(`Connected to the wallet address ${wallet.address}`);
-    const signer = wallet.connect(provider);
-    const balance = await signer.getBalance();
-    console.log(`Wallet balance: ${balance} Wei`);
+    if ( !pkey || pkey.length <= 0) {
+        throw new Error("Missing environment : mnemonic seed");
+    }
 
-    console.log("Deploying Ballot contract");
-    console.log("Proposals: ");
-    proposals.forEach((element, index) => {
-        console.log(`Proposal N. ${index + 1}: ${element}`);
-    });
+    //Get a provider
+    const provider = ethers.getDefaultProvider("goerli");
 
-    const ballotContractFactory = new Ballot__factory(signer);
-    console.log("Deploying contract ...");
+    //Get your signer from .env ( chairperson)
+    const wallet = new ethers.Wallet(pkey);
+    const signer1= wallet.connect(provider);
+
+    //create a contract instance (attach)
+    const ballotContractFactory = new Ballot__factory(signer1);
     const ballotContract = await ballotContractFactory.deploy(
-        convertStringArrayToBytes32(proposals)
+        convertStringArrayToBytes32(PROPOSALS)
     );
-    const deployTxReceipt = await ballotContract.deployTransaction.wait();
-    console.log(
-        `The Ballot contract was deployed at the address ${ballotContract.address}`
-    );
-    console.log({ deployTxReceipt });
+
+    console.log("Deploying contract ...");
+    await ballotContract.deployTransaction.wait();
+    console.log(`The contract was deployed at the address ${ballotContract.address}`);
+
 }
 
 main().catch((error) => {
     console.error(error);
     process.exitCode = 1;
 });
+
+
