18714cd461a2a678e63577cd0fb593c56e096232
uri011 <62013927+uri011@users.noreply.github.com>
2023-03-06 00:37:49 +0100

    Update Tasks Import

--- weekend03/hardhat.config.ts
+++ weekend03/hardhat.config.ts
@@ -1,6 +1,10 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+import './tasks/accounts'
+import './tasks/info'
+import './tasks/network'
+
 const config: HardhatUserConfig = {
   paths: { tests: "tests" },
   solidity: "0.8.17",
